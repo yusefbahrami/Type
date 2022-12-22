5b8f223fca40e84ceef9d6be171d562de1d3c1f1
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-12-22 20:50:59 +0330

    data types and function

---
+++ 2.data_types/JScript.js
@@ -0,0 +1,7 @@
+// var phone: number = 0917
+// let firstName: string = 'ALAEDDIN'
+// const isTrue: boolean = false
+function add(n1, n2) {
+  return n1 + n2;
+}
+console.log(add(4, 3));
---
+++ 2.data_types/TScript.ts
@@ -0,0 +1,9 @@
+// var phone: number = 0917
+// let firstName: string = 'ALAEDDIN'
+// const isTrue: boolean = false
+
+function add(n1: number, n2: number) {
+  return n1 + n2
+}
+
+console.log(add(4, 3))
