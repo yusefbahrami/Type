64e230d59b34500ff24853d6bda21ea5ccb05aa5
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-12-26 03:35:01 +0330

    objects in TS

---
+++ 5.object_types/JScript.js
@@ -0,0 +1,8 @@
+var Person;
+Person = {
+    name: 'ALAEDDIN',
+    age: 20,
+    isMale: true
+};
+console.log(Person);
+console.log(Person.name);
---
+++ 5.object_types/TScript.ts
@@ -0,0 +1,14 @@
+let Person: {
+  name: string
+  age: number
+  isMale: boolean
+}
+
+Person = {
+  name: 'ALAEDDIN',
+  age: 20,
+  isMale: true,
+}
+
+console.log(Person)
+console.log(Person.name)
