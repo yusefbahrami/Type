74ab9b937d4f5804e64d0ed83a452aa0d2bd2975
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-12-26 19:08:07 +0330

    array

---
+++ 6.array/JScript.js
@@ -0,0 +1,14 @@
+var stringList;
+stringList = ['yusef.b', 'ali', 'nuphy air 75'];
+console.log(stringList);
+var numberList;
+numberList = [1, 4, 12];
+console.log(numberList);
+console.log(numberList[1]);
+var randomList;
+randomList = ['ALAEDDIN', 20, false];
+console.log(randomList);
+for (var _i = 0, randomList_1 = randomList; _i < randomList_1.length; _i++) {
+    var item = randomList_1[_i];
+    console.log(item);
+}
---
+++ 6.array/TScript.ts
@@ -0,0 +1,16 @@
+let stringList: string[]
+stringList = ['yusef.b', 'ali', 'nuphy air 75']
+console.log(stringList)
+
+let numberList: number[]
+numberList = [1, 4, 12]
+console.log(numberList)
+console.log(numberList[1])
+
+let randomList: any[]
+randomList = ['ALAEDDIN', 20, false]
+console.log(randomList)
+
+for (const item of randomList) {
+  console.log(item)
+}
