b6c35dff8f9a013007aa8a1ffdc606de89988399
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-12-27 18:08:18 +0330

    tuple

---
+++ 7.tuple/JScript.js
@@ -0,0 +1,4 @@
+var tupleExample;
+tupleExample = ['yusef.b', 20, false];
+tupleExample[0] = 'ALAEDDIN';
+console.log(tupleExample);
---
+++ 7.tuple/TScript.ts
@@ -0,0 +1,4 @@
+let tupleExample: [string, number, boolean]
+tupleExample = ['yusef.b', 20, false]
+tupleExample[0] = 'ALAEDDIN'
+console.log(tupleExample)
